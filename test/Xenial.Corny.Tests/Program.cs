7380730e9e4800f3d12e5a82385555edbe10104d
Manuel Grundner <m.grundner@delegate.at>
2020-11-21 19:16:05 +0100

    feat: textbox tests

--- test/Xenial.Corny.Tests/Program.cs
+++ test/Xenial.Corny.Tests/Program.cs
@@ -1,6 +1,8 @@
 ﻿using static Xenial.Delicious.Corny.Tests.CheckboxFacts;
+using static Xenial.Delicious.Corny.Tests.TextboxFacts;
 using static Xenial.Tasty;
 
 CheckboxTests();
+TextboxTests();
 
 await Run(args);
--- test/Xenial.Corny.Tests/TextboxTests.cs
+++ test/Xenial.Corny.Tests/TextboxTests.cs
@@ -1,298 +1,162 @@
 ﻿using System;
 
+using Cornichon;
+
 using Shouldly;
+
+using Xenial.Delicious.Corny.Tests.TestableControls;
+
+using static Xenial.Tasty;
 
 namespace Xenial.Delicious.Corny.Tests
 {
-    public class TextboxTests
+    public static class TextboxFacts
     {
-        [Scenario]
-        public void LeftArrowScrenario(Textbox textbox)
+        public static void TextboxTests() => Describe(nameof(Textbox), () =>
         {
-            "Given I have a Textbox"
-                .x(() => textbox = new TestableTextbox
+            Textbox CreateTextbox()
+                => new TestableTextbox { Width = 20 };
+
+            void Keypress(Textbox textbox, ConsoleKey direction, char @char = ' ')
+                => ((ISupportKeyPress)textbox).OnKeyPress(new KeyPressEventArgs(new ConsoleKeyInfo(@char, direction, false, false, false)));
+
+            Describe("Cursor navigation", () =>
+            {
+                It("left arrow", () =>
                 {
-                    Width = 20,
+                    var textbox = CreateTextbox();
+                    Scenario
+                        .Given(() => textbox.CursorPosition.ShouldBe(0))
+                        .And(() => Keypress(textbox, ConsoleKey.LeftArrow))
+                        .Then(() => textbox.CursorPosition.ShouldBe(0));
                 });
 
-            "The initial CursorPosition is 0"
-                .x(() => textbox.CursorPosition.ShouldBe(0));
-
-            "When i press left"
-                .x(() => ((ISupportKeyPress)textbox).OnKeyPress(new KeyPressEventArgs(new ConsoleKeyInfo(' ', ConsoleKey.LeftArrow, false, false, false))));
-
-            "The CursorPosition is still 0"
-                .x(() => textbox.CursorPosition.ShouldBe(0));
-        }
-
-        [Scenario]
-        public void RightArrowScrenario(Textbox textbox)
-        {
-            "Given I have a Textbox"
-                .x(() => textbox = new TestableTextbox
+                It("right arrow", () =>
                 {
-                    Width = 20,
+                    var textbox = CreateTextbox();
+                    Scenario
+                        .Given(() => textbox.CursorPosition.ShouldBe(0))
+                        .And(() => Keypress(textbox, ConsoleKey.RightArrow))
+                        .Then(() => textbox.CursorPosition.ShouldBe(0));
                 });
 
-            "The initial CursorPosition is 0"
-                .x(() => textbox.CursorPosition.ShouldBe(0));
-
-            "When i press right"
-                .x(() => ((ISupportKeyPress)textbox).OnKeyPress(new KeyPressEventArgs(new ConsoleKeyInfo(' ', ConsoleKey.RightArrow, false, false, false))));
-
-            "The CursorPosition is still 0"
-                .x(() => textbox.CursorPosition.ShouldBe(0));
-        }
-
-        [Scenario]
-        public void ComplexArrowScrenarioWithText(Textbox textbox)
-        {
-            "Given I have a Textbox"
-                .x(() => textbox = new TestableTextbox
+                It("complex", () =>
                 {
-                    Width = 20,
+                    var textbox = CreateTextbox();
+                    Scenario
+                        .Given(() => textbox.CursorPosition.ShouldBe(0))
+                        .And(() => Keypress(textbox, ConsoleKey.RightArrow))
+                        .Then(() => textbox.CursorPosition.ShouldBe(0))
+                        .And(() => Keypress(textbox, ConsoleKey.A, 'a'))
+                        .Then(() => textbox.CursorPosition.ShouldBe(1))
+                        .And(() => Keypress(textbox, ConsoleKey.RightArrow))
+                        .Then(() => textbox.CursorPosition.ShouldBe(1))
+                        ;
                 });
 
-            "The initial CursorPosition is 0"
-                .x(() => textbox.CursorPosition.ShouldBe(0));
-
-            "When i press right"
-                .x(() => ((ISupportKeyPress)textbox).OnKeyPress(new KeyPressEventArgs(new ConsoleKeyInfo(' ', ConsoleKey.RightArrow, false, false, false))));
-
-            "The CursorPosition is still 0"
-                .x(() => textbox.CursorPosition.ShouldBe(0));
-
-            "When i enter a"
-                .x(() => ((ISupportKeyPress)textbox).OnKeyPress(new KeyPressEventArgs(new ConsoleKeyInfo('a', ConsoleKey.A, false, false, false))));
-
-            "The CursorPosition is 1"
-                .x(() => textbox.CursorPosition.ShouldBe(1));
-
-            "When i press right again"
-               .x(() => ((ISupportKeyPress)textbox).OnKeyPress(new KeyPressEventArgs(new ConsoleKeyInfo(' ', ConsoleKey.RightArrow, false, false, false))));
-
-            "The CursorPosition is still 1"
-                .x(() => textbox.CursorPosition.ShouldBe(1));
-        }
-
-        [Scenario]
-        public void Pos1Scenario(Textbox textbox)
-        {
-            "Given I have a Textbox"
-                .x(() => textbox = new TestableTextbox
+                It("Pos1", () =>
                 {
-                    Width = 20,
+                    var textbox = CreateTextbox();
+                    Scenario
+                        .Given(() => textbox.CursorPosition.ShouldBe(0))
+                        .And(() => Keypress(textbox, ConsoleKey.A, 'a'))
+                        .And(() => Keypress(textbox, ConsoleKey.B, 'b'))
+                        .Then(() => textbox.CursorPosition.ShouldBe(2))
+                        .And(() => Keypress(textbox, ConsoleKey.Home))
+                        .Then(() => textbox.CursorPosition.ShouldBe(0))
+                        ;
                 });
 
-            "The initial CursorPosition is 0"
-                .x(() => textbox.CursorPosition.ShouldBe(0));
-
-            "When i enter a"
-                .x(() => ((ISupportKeyPress)textbox).OnKeyPress(new KeyPressEventArgs(new ConsoleKeyInfo('a', ConsoleKey.A, false, false, false))));
-
-            "And then b"
-                .x(() => ((ISupportKeyPress)textbox).OnKeyPress(new KeyPressEventArgs(new ConsoleKeyInfo('b', ConsoleKey.B, false, false, false))));
-
-            "The CursorPosition is 2"
-                .x(() => textbox.CursorPosition.ShouldBe(2));
-
-            "When i Press Pos1"
-                .x(() => ((ISupportKeyPress)textbox).OnKeyPress(new KeyPressEventArgs(new ConsoleKeyInfo(' ', ConsoleKey.Home, false, false, false))));
-
-            "The CursorPosition is 0"
-                .x(() => textbox.CursorPosition.ShouldBe(0));
-        }
-
-        [Scenario]
-        public void EndScenario(Textbox textbox)
-        {
-            "Given I have a Textbox"
-                .x(() => textbox = new TestableTextbox
+                It("End", () =>
                 {
-                    Width = 20,
+                    var textbox = CreateTextbox();
+                    Scenario
+                        .Given(() => textbox.CursorPosition.ShouldBe(0))
+                        .And(() => Keypress(textbox, ConsoleKey.A, 'a'))
+                        .And(() => Keypress(textbox, ConsoleKey.B, 'b'))
+                        .Then(() => textbox.CursorPosition = 0)
+                        .And(() => Keypress(textbox, ConsoleKey.End))
+                        .Then(() => textbox.CursorPosition.ShouldBe(2))
+                        ;
                 });
 
-            "The initial CursorPosition is 0"
-                .x(() => textbox.CursorPosition.ShouldBe(0));
-
-            "When i enter a"
-                .x(() => ((ISupportKeyPress)textbox).OnKeyPress(new KeyPressEventArgs(new ConsoleKeyInfo('a', ConsoleKey.A, false, false, false))));
-
-            "And then b"
-                .x(() => ((ISupportKeyPress)textbox).OnKeyPress(new KeyPressEventArgs(new ConsoleKeyInfo('b', ConsoleKey.B, false, false, false))));
-
-            "When i set the CursorPosition to 0"
-                .x(() => textbox.CursorPosition = 0);
-
-            "When i Press End"
-                .x(() => ((ISupportKeyPress)textbox).OnKeyPress(new KeyPressEventArgs(new ConsoleKeyInfo(' ', ConsoleKey.End, false, false, false))));
-
-            "The CursorPosition is 2"
-                .x(() => textbox.CursorPosition.ShouldBe(2));
-        }
-
-        [Scenario]
-        public void DelScenario(Textbox textbox)
-        {
-            "Given I have a Textbox"
-                .x(() => textbox = new TestableTextbox
+                It("Del", () =>
                 {
-                    Width = 20,
+                    var textbox = CreateTextbox();
+                    Scenario
+                        .Given(() => textbox.CursorPosition.ShouldBe(0))
+                        .And(() => Keypress(textbox, ConsoleKey.A, 'a'))
+                        .And(() => Keypress(textbox, ConsoleKey.B, 'b'))
+                        .And(() => Keypress(textbox, ConsoleKey.Delete))
+                        .Then(() => textbox.CursorPosition.ShouldBe(2))
+                        .Then(() => textbox.Text.ShouldBe("ab"))
+                        .And(() => Keypress(textbox, ConsoleKey.LeftArrow))
+                        .And(() => Keypress(textbox, ConsoleKey.Delete))
+                        .Then(() => textbox.CursorPosition.ShouldBe(1))
+                        .Then(() => textbox.Text.ShouldBe("a"))
+                        ;
                 });
 
-            "The initial CursorPosition is 0"
-                .x(() => textbox.CursorPosition.ShouldBe(0));
-
-            "When i enter a"
-                .x(() => ((ISupportKeyPress)textbox).OnKeyPress(new KeyPressEventArgs(new ConsoleKeyInfo('a', ConsoleKey.A, false, false, false))));
-
-            "And then b"
-                .x(() => ((ISupportKeyPress)textbox).OnKeyPress(new KeyPressEventArgs(new ConsoleKeyInfo('b', ConsoleKey.B, false, false, false))));
-
-            "When i Press Del"
-                .x(() => ((ISupportKeyPress)textbox).OnKeyPress(new KeyPressEventArgs(new ConsoleKeyInfo(' ', ConsoleKey.Delete, false, false, false))));
-
-            "The CursorPosition is 2"
-                .x(() => textbox.CursorPosition.ShouldBe(2));
-
-            "And the Text is still ab"
-                .x(() => textbox.Text.ShouldBe("ab"));
-
-            "But when i press left"
-                .x(() => ((ISupportKeyPress)textbox).OnKeyPress(new KeyPressEventArgs(new ConsoleKeyInfo(' ', ConsoleKey.LeftArrow, false, false, false))));
-
-            "And then Del"
-                .x(() => ((ISupportKeyPress)textbox).OnKeyPress(new KeyPressEventArgs(new ConsoleKeyInfo(' ', ConsoleKey.Delete, false, false, false))));
-
-            "The CursorPosition is 1"
-                .x(() => textbox.CursorPosition.ShouldBe(1));
-
-            "And the Text is a"
-                .x(() => textbox.Text.ShouldBe("a"));
-        }
-
-        [Scenario]
-        public void ComplexInsertScenario(Textbox textbox)
-        {
-            "Given I have a Textbox"
-                .x(() => textbox = new TestableTextbox
+                Describe("Backspace", () =>
                 {
-                    Width = 20,
+                    It("simple", () =>
+                    {
+                        var textbox = CreateTextbox();
+                        Scenario
+                            .Given(() => textbox.CursorPosition.ShouldBe(0))
+                            .And(() => Keypress(textbox, ConsoleKey.Backspace))
+                            .Then(() => textbox.CursorPosition.ShouldBe(0))
+                            ;
+                    });
+
+                    It("with text", () =>
+                    {
+                        var textbox = CreateTextbox();
+                        Scenario
+                            .Given(() => textbox.CursorPosition.ShouldBe(0))
+                            .And(() => Keypress(textbox, ConsoleKey.A, 'a'))
+                            .And(() => Keypress(textbox, ConsoleKey.B, 'b'))
+                            .And(() => Keypress(textbox, ConsoleKey.C, 'c'))
+                            .Then(() => textbox.Text.ShouldBe("abc"))
+                            .And(() => Keypress(textbox, ConsoleKey.Backspace))
+                            .Then(() => textbox.Text.ShouldBe("ab"))
+                            ;
+                    });
+
+                    It("with text and movement", () =>
+                    {
+                        var textbox = CreateTextbox();
+                        Scenario
+                            .Given(() => textbox.CursorPosition.ShouldBe(0))
+                            .And(() => Keypress(textbox, ConsoleKey.A, 'a'))
+                            .And(() => Keypress(textbox, ConsoleKey.B, 'b'))
+                            .And(() => Keypress(textbox, ConsoleKey.C, 'c'))
+                            .Then(() => textbox.Text.ShouldBe("abc"))
+                            .And(() => Keypress(textbox, ConsoleKey.LeftArrow))
+                            .And(() => Keypress(textbox, ConsoleKey.Backspace))
+                            .Then(() => textbox.Text.ShouldBe("ac"))
+                            .Then(() => textbox.CursorPosition.ShouldBe(1))
+                            ;
+                    });
                 });
 
-            "The initial CursorPosition is 0"
-                .x(() => textbox.CursorPosition.ShouldBe(0));
+            });
 
-            "When i enter a"
-                .x(() => ((ISupportKeyPress)textbox).OnKeyPress(new KeyPressEventArgs(new ConsoleKeyInfo('a', ConsoleKey.A, false, false, false))));
-
-            "And then b"
-                .x(() => ((ISupportKeyPress)textbox).OnKeyPress(new KeyPressEventArgs(new ConsoleKeyInfo('b', ConsoleKey.B, false, false, false))));
-
-            "But when i press left"
-               .x(() => ((ISupportKeyPress)textbox).OnKeyPress(new KeyPressEventArgs(new ConsoleKeyInfo(' ', ConsoleKey.LeftArrow, false, false, false))));
-
-            "The CursorPosition is 1"
-                .x(() => textbox.CursorPosition.ShouldBe(1));
-
-            "And the Text is still ab"
-                .x(() => textbox.Text.ShouldBe("ab"));
-
-            "When i now hit c"
-                 .x(() => ((ISupportKeyPress)textbox).OnKeyPress(new KeyPressEventArgs(new ConsoleKeyInfo('c', ConsoleKey.B, false, false, false))));
-
-            "The CursorPosition is 2"
-                .x(() => textbox.CursorPosition.ShouldBe(2));
-
-            "And the Text is acb"
-                .x(() => textbox.Text.ShouldBe("acb"));
-        }
-
-        [Scenario]
-        public void BackspaceScrenario(Textbox textbox)
-        {
-            "Given I have a Textbox"
-                .x(() => textbox = new TestableTextbox
-                {
-                    Width = 20,
-                });
-
-            "The initial CursorPosition is 0"
-                .x(() => textbox.CursorPosition.ShouldBe(0));
-
-            "When i press left"
-                .x(() => ((ISupportKeyPress)textbox).OnKeyPress(new KeyPressEventArgs(new ConsoleKeyInfo(' ', ConsoleKey.Backspace, false, false, false))));
-
-            "The CursorPosition is still 0"
-                .x(() => textbox.CursorPosition.ShouldBe(0));
-        }
-
-        [Scenario]
-        public void BackspaceWithTextScenario(Textbox textbox)
-        {
-            "Given I have a Textbox"
-                .x(() => textbox = new TestableTextbox
-                {
-                    Width = 20,
-                });
-
-            "The initial CursorPosition is 0"
-                .x(() => textbox.CursorPosition.ShouldBe(0));
-
-            "When i enter a"
-                .x(() => ((ISupportKeyPress)textbox).OnKeyPress(new KeyPressEventArgs(new ConsoleKeyInfo('a', ConsoleKey.A, false, false, false))));
-
-            "And then b"
-                .x(() => ((ISupportKeyPress)textbox).OnKeyPress(new KeyPressEventArgs(new ConsoleKeyInfo('b', ConsoleKey.B, false, false, false))));
-
-            "And then c"
-                .x(() => ((ISupportKeyPress)textbox).OnKeyPress(new KeyPressEventArgs(new ConsoleKeyInfo('c', ConsoleKey.C, false, false, false))));
-
-            "Text is abc"
-               .x(() => textbox.Text.ShouldBe("abc"));
-
-            "When I hit Backspace now"
-               .x(() => ((ISupportKeyPress)textbox).OnKeyPress(new KeyPressEventArgs(new ConsoleKeyInfo(' ', ConsoleKey.Backspace, false, false, false))));
-
-            "Text is ab"
-               .x(() => textbox.Text.ShouldBe("ab"));
-        }
-
-        [Scenario]
-        public void BackspaceWithTextAndMovementScenario(Textbox textbox)
-        {
-            "Given I have a Textbox"
-                .x(() => textbox = new TestableTextbox
-                {
-                    Width = 20,
-                });
-
-            "The initial CursorPosition is 0"
-                .x(() => textbox.CursorPosition.ShouldBe(0));
-
-            "When i enter a"
-                .x(() => ((ISupportKeyPress)textbox).OnKeyPress(new KeyPressEventArgs(new ConsoleKeyInfo('a', ConsoleKey.A, false, false, false))));
-
-            "And then b"
-                .x(() => ((ISupportKeyPress)textbox).OnKeyPress(new KeyPressEventArgs(new ConsoleKeyInfo('b', ConsoleKey.B, false, false, false))));
-
-            "And then c"
-                .x(() => ((ISupportKeyPress)textbox).OnKeyPress(new KeyPressEventArgs(new ConsoleKeyInfo('c', ConsoleKey.C, false, false, false))));
-
-            "Text is abc"
-               .x(() => textbox.Text.ShouldBe("abc"));
-
-            "When i press left"
-               .x(() => ((ISupportKeyPress)textbox).OnKeyPress(new KeyPressEventArgs(new ConsoleKeyInfo(' ', ConsoleKey.LeftArrow, false, false, false))));
-
-            "And then the Backspace key"
-               .x(() => ((ISupportKeyPress)textbox).OnKeyPress(new KeyPressEventArgs(new ConsoleKeyInfo(' ', ConsoleKey.Backspace, false, false, false))));
-
-            "Text is ac"
-               .x(() => textbox.Text.ShouldBe("ac"));
-
-            "And the CursorPosition should be 1"
-               .x(() => textbox.CursorPosition.ShouldBe(1));
-        }
+            It("inserts text with complex movement", () =>
+            {
+                var textbox = CreateTextbox();
+                Scenario
+                    .Given(() => textbox.CursorPosition.ShouldBe(0))
+                    .And(() => Keypress(textbox, ConsoleKey.A, 'a'))
+                    .And(() => Keypress(textbox, ConsoleKey.B, 'b'))
+                    .And(() => Keypress(textbox, ConsoleKey.LeftArrow))
+                    .Then(() => textbox.CursorPosition.ShouldBe(1))
+                    .Then(() => textbox.Text.ShouldBe("ab"))
+                    .And(() => Keypress(textbox, ConsoleKey.C, 'c'))
+                    .Then(() => textbox.CursorPosition.ShouldBe(2))
+                    .Then(() => textbox.Text.ShouldBe("acb"))
+                    ;
+            });
+        });
     }
 }
