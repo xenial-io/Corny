03c7be0a849a60870aab3b7a964bb108f02ad15e
Manuel Grundner <m.grundner@delegate.at>
2020-11-24 14:16:23 +0100

    feat: added char record

---
+++ src/Xenial.Corny/Data/Character.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xenial.Delicious.Corny.Data
+{
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Is record type")]
+    public record Character(char? Content, Color? Foreground, Color? Background)
+    {
+        public static readonly Character Empty = new Character(null, null, null);
+    }
+}
